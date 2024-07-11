795a59aa3b0accf685b896e28462d881d79bf159
eltonanit <elton.ani@gmail.com>
2024-07-11 09:44:42 +0200

    [UPD] Added event fucnction button at gioca

--- PARI-DISPARI/script2.js
+++ PARI-DISPARI/script2.js
@@ -4,3 +4,16 @@
 function generateRandomNumber() {
     return Math.floor(Math.random() * 5) + 1;
 }
+
+
+// Funzione per verificare se un numero è pari
+function Even(number) {
+    return number % 2 === 0;
+}
+
+// Aggiunto evento a bottone per il id del bottone gioca 
+
+document.getElementById('gioca').addEventListener('click', function () {
+    let user = document.getElementById('scelta').value;
+    let usernumber = parseInt(document.getElementById('usernumber').value);
+   
